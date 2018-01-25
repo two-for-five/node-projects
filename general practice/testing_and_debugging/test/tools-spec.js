eca4d4fa38743e59abded4f50c6599fa65df7f6b
Yifeng Liu <yifeng.liu@duke.edu>
2018-01-24 20:31:29 -0500

    mocha chai nock practice

---
+++ general practice/testing_and_debugging/test/tools-spec.js
@@ -0,0 +1,28 @@
+
+var expect = require("chai").expect;
+var tools = require("../lib/tools");
+var nock = require("nock");
+describe("Tools", function(){
+
+	describe("printName()", function(){
+		it("should print the last name first", function(){
+			var results = tools.printName({first:"Alex", last:"Banks"});
+			expect(results).to.equal("Banks, Alex");
+		});
+	})
+
+	describe("loadWiki()", function(){
+		before(function(){
+			nock("https://en.wikipedia.org").get("/wiki/Abraham_Lincoln").reply(200, "Mock Lincoln");
+		})
+		it("load Lincoln's wikipedia page", function(done){
+
+			tools.loadWiki({first:"Abraham", last:"Lincoln"}, function(html){
+				expect(html).to.equal("Mock Lincoln");
+				done();
+			});
+		});
+	})
+
+
+})
